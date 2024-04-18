22980ed2ae8af72d829d2c3e5963ddfb504cce1a
emamaj <edyta.m.info@gmail.com>
2024-04-18 16:27:41 +0200

    run script added

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -3,11 +3,10 @@ import { test, expect } from '@playwright/test';
 test.describe('Pulpit tests', () => {
   
   test.beforeEach(async ({ page }) => {
-    const url = ;
     const userID = 'testerLO';
     const userPassword = 'password';
 
-    await page.goto(url);
+    await page.goto('/');
     await page.getByTestId('login-input').fill(userID);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
@@ -15,7 +14,6 @@ test.describe('Pulpit tests', () => {
 
   test('Quick payment with correct data', async ({ page }) => {
     //Arrange
-
     const receiverID = '2';
     const transferAmount = '150';
     const transferTitle = 'pizza';
@@ -48,5 +46,23 @@ test.describe('Pulpit tests', () => {
 
     //Assert
     await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
+  });
+
+  test('Correct balance after successful mobile top-up', async ({ page }) => {
+    //Arange
+    const topUpReceiver = '500 xxx xxx';
+    const topUpAmount = '25';
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(topUpAmount);
+
+    //Act
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+    await page.locator('#widget_1_topup_agreement').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    //Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
   });
 });
